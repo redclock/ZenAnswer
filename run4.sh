b66a74e96d3665bcaec91fea0c1651e5768edfe9
chunhui.yao <chunhui.yao@zentertain.net>
2016-03-22 10:40:10 +0800

    add one more test case for #4

--- run4.sh
+++ run4.sh
@@ -13,5 +13,6 @@ run() {
 
 compile \
 && run "A->B\nB->C\nA->C\nD->C" \
+&& run "A->B\nA->C\nB->D\nC->D" \
 && run "A->B\nB->C\nC->A\nD->C" \
 && run "戴眼镜 -> 刷牙\n戴眼镜 -> 洗脸\n戴眼镜 -> 穿t恤\n穿袜子 -> 穿鞋\n穿t恤 -> 穿外套\n穿裤子 -> 穿袜子\n穿裤子 -> 穿鞋\n刷牙  -> 洗脸\n刷牙  -> 吃早饭\n" \
